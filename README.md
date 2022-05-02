e4d0bf427d96736343d33db03db28071eb5a232c
SangYeol-github <polkmn222@naver.com>
2022-05-02 20:48:05 +0900

    macro

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 macro   
 zip.py : 알집 비밀번호 풀기 macro   
 nox.py : 녹스 자동 클릭, 입력 macro   
+hotkey.py : 버튼 자동 입력 macro      
---
+++ hotkey.py
@@ -0,0 +1,10 @@
+import pyautogui
+import keyboard
+
+# pip install pyautogui
+# pip install keyboard
+for i in range(10):
+    pyautogui.hotkey('g', interval=0.05)
+    
+    
+    
